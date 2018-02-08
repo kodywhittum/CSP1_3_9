56a820236651ae1541a2a41af924c245d932e2d3
kodywhittum <kody.whittum@sau36.org>
2018-02-08 12:54:05 -0500

    Team 4 Names
    Individual and team name

--- team4.py
+++ team4.py
@@ -6,7 +6,7 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
+team_name = 'Brandon has the brain of a Freshmen' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
